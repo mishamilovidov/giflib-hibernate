5130e81430e41a229d4193d6280878ea5fa7d739
Misha Milovidov <mmilovidov@doterra.com>
2017-06-07 14:58:07 -0600

    Added data layer; started implementing Dao and service within the application layer

--- src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
@@ -1,6 +1,10 @@
 package com.teamtreehouse.giflib.web.controller;
 
 import com.teamtreehouse.giflib.model.Category;
+import com.teamtreehouse.giflib.service.CategoryService;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +17,15 @@ import java.util.List;
 @Controller
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     // Index of all categories
     @RequestMapping("/categories")
+    @SuppressWarnings("unchecked")
     public String listCategories(Model model) {
         // TODO: Get all categories
-        List<Category> categories = new ArrayList<>();
+        List<Category> categories = categoryService.findAll();
 
         model.addAttribute("categories",categories);
         return "category/index";
